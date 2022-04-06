2343da8478a8cf239f9cbceecdec39280e74f591
DevSin <48406477+Stylezaa@users.noreply.github.com>
2022-04-06 15:03:14 +0700

    Update LocaleSwitcher.vue

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -158,7 +158,7 @@ div#sw_lang_section {
     left: 170px;  
     display: flex;
     align-items: center;
-    justify-content: end !important ;
+    justify-content: flex-end !important ;
     align-items: center;
   }
 }
